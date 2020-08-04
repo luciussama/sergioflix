0e27e625ae0a4739ac00ca07aaf7f408692edd15
Sergio Roberto Garcia Rodrigues Filho <54035472+luciussama@users.noreply.github.com>
2020-08-04 09:07:49 -0300

    - Término do FormField

--- src/pages/cadastro/categoria/index.js
+++ src/pages/cadastro/categoria/index.js
@@ -56,7 +56,7 @@ function CadastroCategoria() {
 
         <FormField
           label="Descrição:"
-          type="????"
+          type="textarea"
           name="descricao"
           value={values.descricao}
           onChange={handleChange}
